c254169a6c8808d4c344dced2184b77fe8eceacf
Xiaodong_Wang <dongzhixiaodong@gmail.com>
2016-10-02 18:40:27 +0800

    modify some programs

--- SalaryRaise_ResponsibilityEven_mode_24/manager.h
+++ SalaryRaise_ResponsibilityEven_mode_24/manager.h
@@ -3,15 +3,16 @@
 
 #include <QString>
 #include <QSharedPointer>
+#include <QDebug>
 
 class Request
 {
 public:
-    QString getRequestType(){return _requestType;}
+    QString getRequestType() const {return _requestType;}
     void setRequestType(QString requestType){_requestType = requestType;}
-    QString getRequestContent(){return _requestContent;}
+    QString getRequestContent() const {return _requestContent;}
     void setRequestContent(QString requestContent){_requestContent = requestContent;}
-    int getNumber(){return _number;}
+    int getNumber() const {return _number;}
     void setNumber(int number){_number = number;}
 private:
     QString _requestType;
@@ -37,6 +38,62 @@ protected:
     QSharedPointer<Manager> _superior;
 };
 
+class CommonManager final : public Manager   //经理类
+{
+public:
+    explicit CommonManager(QString name):Manager(name){}
+    void RequestApplications(const Request & request) override
+    //注意，参数如果有const实例，则函数里面调用该实例的方法必须是const函数
+    {
+        if(request.getRequestType() == "请假" && request.getNumber()<=2)
+        {
+            qDebug()<<_name<<request.getRequestContent()<<request.getNumber()<<"被批准";
+        }
+        else
+        {
+            if(_superior!=nullptr)
+                _superior->RequestApplications(request);
+        }
+    }
+};
 
+class Majordomo final : public Manager   //总监类
+{
+public:
+    explicit Majordomo(QString name):Manager(name){}
+    void RequestApplications(const Request & request) override
+    {
+        if(request.getRequestType() == "请假" && request.getNumber()<=5)
+        {
+            qDebug()<<_name<<request.getRequestContent()<<request.getNumber()<<"被批准";
+        }
+        else
+        {
+            if(_superior!=nullptr)
+                _superior->RequestApplications(request);
+        }
+    }
+};
+
+class GeneralManager final : public Manager   //总经理类
+{
+public:
+    explicit GeneralManager(QString name):Manager(name){}
+    void RequestApplications(const Request & request) override
+    {
+        if(request.getRequestType() == "请假" && request.getNumber()<=500)
+        {
+            qDebug()<<_name<<request.getRequestContent()<<request.getNumber()<<"被批准";
+        }
+        else if(request.getRequestType() == "加薪" && request.getNumber()<=500)
+        {
+           qDebug()<<_name<<request.getRequestContent()<<request.getNumber()<<"被批准";
+        }
+        else if(request.getRequestType() == "加薪" && request.getNumber()>500)
+        {
+            qDebug()<<_name<<request.getRequestContent()<<request.getNumber()<<"再说吧";
+        }
+    }
+};
 
 #endif // MANAGER_H
