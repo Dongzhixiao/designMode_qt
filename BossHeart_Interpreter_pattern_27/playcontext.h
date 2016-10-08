c2509e6602303cc3b51c4b168b1b6d45837b9500
Xiaodong_Wang <dongzhixiaodong@gmail.com>
2016-10-08 18:53:17 +0800

    modify some programs

--- BossHeart_Interpreter_pattern_27/playcontext.h
+++ BossHeart_Interpreter_pattern_27/playcontext.h
@@ -2,6 +2,7 @@
 #define PLAYCONTEXT_H
 
 #include <QString>
+#include <QDebug>
 
 class PlayContext   //演奏内容类
 {
@@ -23,8 +24,69 @@ class Expression   //表达式类
 public:
     void Interpret(PlayContext* context)
     {
+        if(context->GetPlayText().length() == 0)
+            return;
+        else
+        {
+            QString playKey = context->GetPlayText().section(' ',0);
+            int playValue = context->GetPlayText().section(' ',1).toInt();
+            context->SetPlayText(context->GetPlayText().section(' ',2,-1));
 
+            Excute(playKey,playValue);
+        }
+    }
+    virtual void Excute(QString,int) = 0;
+};
+
+class Note final : public Expression   //音符类
+{
+public:
+    void Excute(QString key,int value) override
+    {
+        QString note;
+        if(key == "C")
+            note = '1';
+        else if(key == "D")
+            note = '2';
+        else if(key == "E")
+            note = '3';
+        else if(key == "F")
+            note = '4';
+        else if(key == "G")
+            note = '5';
+        else if(key == "A")
+            note = '6';
+        else if(key == "B")
+            note = '7';
+
+        qDebug()<<note;
     }
 };
+
+class Scale final : public Expression   //音符类
+{
+public:
+    void Excute(QString key,int value) override
+    {
+        QString scale;
+
+        switch (value) {
+        case 1:
+            scale = "低音";
+            break;
+        case 2:
+            scale = "中音";
+            break;
+        case 3:
+            scale = "高音";
+            break;
+        default:
+            break;
+        }
+
+        qDebug()<<scale;
+    }
+};
+
 
 #endif // PLAYCONTEXT_H
